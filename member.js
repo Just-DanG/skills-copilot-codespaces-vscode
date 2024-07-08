853b53006d7c5f59a29113f21f9f9b99cb5ad6b1
Dan G <galvindan61@gmail.com>
2024-07-08 04:53:13 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,3 +1,8 @@
 function skillsmember() {
-  console.log("I'm a member of the team")
+  var x = document.getElementById("skills");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
